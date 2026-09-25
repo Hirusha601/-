8ea486079de6524a7f153b4c179dc64abb387378
Hirusha601 <pk3962234@gmail.com>
2026-09-26 03:09:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ async function connectToWA() {
 
       const up = `ʜɪʀᴜᴢ-ᴍᴅ ᴄᴏɴɴᴇᴄᴛᴇᴅ ✅\n\nPREFIX: ${prefix}`;
       await danuwa.sendMessage(ownerNumber[0] + "@s.whatsapp.net", {
-        image: { url: `https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true` },
+        image: { url: `https://github.com/Hirusha601/-/blob/main/images/file_0000000076ac8208809a627406e06ead.png` },
         caption: up
       });
 
@@ -179,7 +179,7 @@ async function connectToWA() {
 ensureSessionFile();
 
 app.get("/", (req, res) => {
-  res.send("Hey, DANUWA-MD started✅");
+  res.send("ʜᴇy, ʜɪʀᴜᴢ-ᴍᴅ ꜱᴛᴀʀᴛᴇᴅ✅");
 });
 
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
